3b3c6451a36409ac88cacd9ebd17495417e251bd
Ingrid <ingrid.munne@citcea.upc.edu>
2021-03-22 17:58:36 +0100

    script for encoding datasets

---
+++ L1_M1_climatology/L1_M1_data_encoding.py
@@ -0,0 +1,22 @@
+# DATAPORT Austin 2018 - Functions for hierarchical modeling
+# Level 1 - Model 1 (Climatology model for probability of Flex YES/NO)
+# Script for data encoding YES/NO
+# Author : Íngrid Munné-Collado
+# Date: 22/03/2021
+
+from L1_M1_climat_funct_main import *
+import pickle
+
+# Define paths
+file_path = '../data/aggregated_2018_austin_corrected.pkl'
+fig_path = '../figures/'
+folder_path = '../results/'
+# Read input data
+df = load_dataset(file_path, granularity = '1H')
+column = ['flex_load_kWh']
+threshold = 0.2
+encoded_df = encode_threshold(df, column, threshold)
+encoded_df.to_csv('../data/L1_M1_encoded_data.csv')
+with open('../data/L1_M1_encoded_data.pkl', 'wb') as f:
+    pickle.dump(encoded_df, f)
+    f.close()
